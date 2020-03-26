a27ed10c4c757c85abc7005b91d47852fd72c176
Piotr Wicijowski <piotr.wicijowski@gmail.com>
2020-03-27 00:06:17 +0100

    Cleanup

--- source/Services/HBoxBufferRenderer.cs
+++ source/Services/HBoxBufferRenderer.cs
@@ -35,42 +35,5 @@ namespace myotui.Services
                 );
             return view;
         }
-        // public override View Render(IBuffer buffer, string scope)
-        // {
-        //     var hboxbuffer = buffer as HBoxBuffer;
-        //     var view = new View();
-        //     var label = new Label(hboxbuffer.Name)
-        //     {
-        //         X = 0 + 1,
-        //         Y = 0,
-        //     };
-        //     view.Add(label);
-
-
-        //     var count = hboxbuffer.Windows.Count(); 
-        //     var dims = GetDims(hboxbuffer.Windows.Select(content => content.Width));
-        //     var elements = hboxbuffer.Windows
-        //         .Select((content,i) => {
-        //             var element = _bufferService.RenderBuffer(content.Value,$"{scope}/{content.Name}");
-        //             RegisterFocusAction(view,element,$"{scope}/{content.Name}");
-        //             return element;
-        //         })
-        //         .Zip(dims, (element, dim) => 
-        //         {
-        //             element.Y = 0;
-        //             element.Height = Dim.Fill();
-        //             element.Width = dim;
-        //             return element;
-        //         })
-        //         .Aggregate<View, View>(null, (last, element) => 
-        //         {
-        //             element.X = last == null ? 0 : Pos.Right(last);
-        //             view.Add(element);
-        //             return element;
-        //         }
-        //         );
-        //     RegisterBindings(hboxbuffer.Bindings, scope);
-        //     return view;
-        // }
     }
 }
