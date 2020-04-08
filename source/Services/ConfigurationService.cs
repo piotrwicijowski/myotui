473d56e50d8727850d4d971d0c4e4f3ddeea3f08
Piotr Wicijowski <piotr.wicijowski@gmail.com>
2020-04-08 23:52:16 +0200

    Added vars to templating, managed to extract "complex buffer" group

--- source/Services/ConfigurationService.cs
+++ source/Services/ConfigurationService.cs
@@ -25,16 +25,27 @@ namespace myotui.Services
         public async Task BuildAppConfiguration(string configDirectory)
         {
             var dirFullPath = Path.GetFullPath(configDirectory);
-            var mainPath = Path.Join(dirFullPath,@"main.yml");
-            var fileStream = new FileStream(mainPath, FileMode.Open);
-            using var reader = new StreamReader(fileStream);
-            var mainContent = await reader.ReadToEndAsync();
 
             var partials = GetPartialsFromDirectory(configDirectory);
             partials.Keys.ToList().ForEach(key => Handlebars.RegisterTemplate(key,partials[key]));
 
+            dynamic vars = new {};
+            var varsPath = Path.Join(dirFullPath,@"vars.yml");
+            if(File.Exists(varsPath))
+            {
+                var varsContent = await File.ReadAllTextAsync(varsPath);
+                var varsTemplate = Handlebars.Compile(varsContent);
+
+                var varsDeserializer = new Deserializer();
+                vars = varsDeserializer.Deserialize<dynamic>(varsTemplate(new {}));
+            }
+
+
+            var mainPath = Path.Join(dirFullPath,@"main.yml");
+            var mainContent = await File.ReadAllTextAsync(mainPath);
+
             var mainTemplate = Handlebars.Compile(mainContent);
-            var renderedMain = mainTemplate(new {});
+            var renderedMain = mainTemplate(vars);
 
             var deserializer = new DeserializerBuilder()
                 .WithNodeTypeResolver(_resolver)
