d87967bbb4377ebe8f7aa08fa6cb3ef198689d3a
Piotr Wicijowski <piotr.wicijowski@gmail.com>
2020-04-18 23:58:08 +0200

    Started work on action list buffer/content

---
+++ source/Models/Config/ActionListElement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace myotui.Models.Config
+{
+    public class ActionListElement
+    {
+        public string DisplayName {get; set;}
+        public string Action {get; set;}
+    }
+}
---
+++ source/Services/ActionListRawContentService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using myotui;
+using myotui.Services;
+using myotui.Models.Config;
+using Newtonsoft.Json;
+
+namespace myotui.Services
+{
+    public class ActionListRawContentService : IRawContentService
+    {
+        protected readonly IParameterService _parameterService;
+        public ActionListRawContentService(IParameterService parameterService)
+        {
+            _parameterService = parameterService;
+        }
+        public string GetRawOutput(IValueContent content, IDictionary<string, string> parameters)
+        {
+            var listContent = content as ActionListValueContent;
+            // var result = _parameterService.SubstituteParameters(string.Join(Environment.NewLine,listContent.Value),parameters);
+            var result = listContent.Value.Select(element => new ActionListElement(){
+                DisplayName = _parameterService.SubstituteParameters(element.DisplayName, parameters),
+                Action = _parameterService.SubstituteParameters(element.Action, parameters),
+            });
+            
+            return JsonConvert.SerializeObject(result);
+        }
+
+    }
+}
