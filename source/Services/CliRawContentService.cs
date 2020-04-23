9d166b517ac6cd96aa48aed0180968535c2e9f3d
Piotr Wicijowski <piotr.wicijowski@avanade.com>
2020-04-23 22:39:01 +0200

    Started work on windows support

--- source/Services/CliRawContentService.cs
+++ source/Services/CliRawContentService.cs
@@ -21,8 +21,14 @@ namespace myotui.Services
             var cliSubstituted = _parameterService.SubstituteParameters(string.Join(Environment.NewLine,cliContent.Input),parameters);
             var commandSplit = StringExtensions.SplitCommandLine(cliSubstituted);
             using var pProcess = new Process();
-            pProcess.StartInfo.FileName = commandSplit.Take(1).FirstOrDefault();
-            pProcess.StartInfo.Arguments = string.Join(' ', commandSplit.Skip(1));
+            var filename = commandSplit.Take(1).FirstOrDefault();
+            // filename = $"\"C:\\Program Files (x86)\\Microsoft SDKs\\Azure\\CLI2\\wbin\\{filename}.cmd\"";
+            // filename = $"{filename}.cmd";
+            var args = string.Join(' ', commandSplit.Skip(1));
+            filename = "cmd.exe";
+            args = "/c " + cliSubstituted;
+            pProcess.StartInfo.FileName = filename;
+            pProcess.StartInfo.Arguments = args;
             pProcess.StartInfo.UseShellExecute = false;
             pProcess.StartInfo.RedirectStandardOutput = true;
             pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
